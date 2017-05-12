93f1a8a1bdf872a9c3d440da7b250b388641c40f
Perego98 <peregopr@gmail.com>
2017-05-12 15:47:36 +0200

    creazione client e server

--- Materiali/JMAppInserimentoCircolareONotizia/SocketServerFile/src/Server.java
+++ Materiali/JMAppInserimentoCircolareONotizia/SocketServerFile/src/Server.java
@@ -1,12 +1,42 @@
+import java.beans.Statement;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
  
  
 public class Server {
  
   private final int SERVER_PORT = 3333;
- 
+ 	// jdbc url per MySql.
+	public static final String URL = "jdbc:mysql://localhost/testcircolare";
+
+	// username per la connessione al database
+	public static final String USERNAME = "root";
+
+	//The password per la connessione al database
+	public static final String PASSWORD = "";
+  
+  
   public static void main(String[] args) {
       Server s = new Server();
       try {
@@ -15,7 +45,55 @@ public class Server {
           e.printStackTrace();
       }
   }
- 
+ private String getNomeFile() throws ClassNotFoundException, SQLException{
+            Connection connection = null;
+            try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Ottenere la connessione dal driver manager
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                        
+
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+            } 
+            
+            //---------------
+             String query = "SELECT nomeCircolare FROM circolari";
+
+      // create the java statement
+      java.sql.Statement st = connection.createStatement();
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = st.executeQuery(query);
+      
+      String result="";
+      while (rs.next())
+      {
+        result = rs.getString("nomeCircolare");
+        /*String firstName = rs.getString("first_name");
+        String lastName = rs.getString("last_name");
+        Date dateCreated = rs.getDate("date_created");
+        boolean isAdmin = rs.getBoolean("is_admin");
+        int numPoints = rs.getInt("num_points");
+        // print the results
+        System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);*/
+        
+      }
+      st.close();
+    /*}
+    catch (Exception e)
+    {
+      System.err.println("Got an exception! ");
+      System.err.println(e.getMessage());
+    }*/
+            
+            //---------------
+       
+            return result;
+ }
   private void listen() throws IOException {
       ServerSocket ss = new ServerSocket(SERVER_PORT);
       System.out.println("Sono sulla " + ss);
